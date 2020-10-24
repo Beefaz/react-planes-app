67af834a6f5b388755166ead1be2fc8730e1f0eb
justinas.geryba@gmail.com <justinas.geryba@gmail.com>
2020-10-24 20:14:34 +0300

    Now video links can be added to needed item in DATA.jsx (video:'url') and it will be displayed in item page. Status can be added to Data.jsx which will show only paragraphs, but not paragraph nmes. All content of item will be shown in order on DATA.jsx

--- src/components/itemPage/ItemPage.jsx
+++ src/components/itemPage/ItemPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Image, Jumbotron} from "react-bootstrap";
 import ImageGrid from "./ImageGrid";
+import Iframe from 'react-iframe';
 
 
 const ItemPage = (props) => {
@@ -9,6 +10,13 @@ const ItemPage = (props) => {
     };
     const paragraphStyle = {
 
+    };
+    const iframeParams={
+        width:"100%",
+        height:"415",
+        frameBorder:"0",
+        allow:"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen",
+        url: props.item.CONTENT.Video,
     };
     const newLinesToComponents = (HTMLComponentString, styleProp, content) => {
         return content.split('\n').map((lineText, index, key) => React.createElement(HTMLComponentString, {
@@ -22,10 +30,10 @@ const ItemPage = (props) => {
                alt=""
                fluid/>
         {Object.keys(props.item.CONTENT).map((paragraphItem, index) => (
-            <div key={'Section'.concat((index).toString())}>
-                {newLinesToComponents('h4', paragraphNameStyle, paragraphItem.replace('_', ' '))}
-                {newLinesToComponents('p', paragraphStyle, props.item.CONTENT[paragraphItem])}
-            </div>
+            <section key={'Section'.concat((index).toString())}>
+                {(paragraphItem !=='Status') && (paragraphItem !=='Video') ? newLinesToComponents('h4', paragraphNameStyle, paragraphItem.replace('_', ' ')) : null}
+                {paragraphItem !=='Video' ? newLinesToComponents('p', paragraphStyle, props.item.CONTENT[paragraphItem]) : React.createElement(Iframe, iframeParams)}
+            </section>
         ),)}
         <ImageGrid item={props.item} id={'Plane gallery'}/>
     </Jumbotron>
